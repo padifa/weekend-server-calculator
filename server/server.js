9406e6bc35bb1480034817e04ad6c664a9a74376
padifa <padifa7@live.fr>
2024-09-07 13:58:26 -0500

    GET/calculations created

--- server/server.js
+++ server/server.js
@@ -1,28 +1,38 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 let PORT = process.env.PORT || 5000;
 
 app.use(express.json());
-app.use(express.static('server/public'));
+app.use(express.static("server/public"));
 
 // Global variable that will contain all of the
 // calculation objects:
-let calculations = []
-
+let calculations = [];
 
 // Here's a wonderful place to make some routes:
 
 // GET /calculations
-
+app.get("/calculations", (req, res) => {
+  console.log("GET, handles calculations route");
+  res.send(calculations);
+});
 // POST /calculations
-
+app.post("/calculations", (req, res) => {
+  console.log(req.body);
+  let result;
+  if (operator === "+") {
+    result = numOne + numTwo;
+  } else if (operator === "-") {
+    result = numOne - numTwo;
+  }
+});
 
 // PLEASE DO NOT MODIFY ANY CODE BELOW THESE BEARS:
 // 🐻  🐻‍❄️  🧸  🐻  🐻‍❄️  🧸  🐻  🐻‍❄️  🧸  🐻  🐻‍❄️  🧸
 
 // Makes it so you don't have to kill the server
 // on 5000 in order to run the tests:
-if (process.env.NODE_ENV === 'test') {
+if (process.env.NODE_ENV === "test") {
   PORT = 5001;
 }
 
@@ -30,7 +40,7 @@ if (process.env.NODE_ENV === 'test') {
 // This is weird. We have to do it for testing reasons. There
 // is absolutely no need for you to reason about this.
 const server = app.listen(PORT, () => {
-  console.log('server running on: ', PORT);
+  console.log("server running on: ", PORT);
 });
 
 // server.setTimeout(500)
@@ -39,10 +49,10 @@ const server = app.listen(PORT, () => {
 // absolutely no need for you to reason about this.
 app.closeServer = () => {
   server.close();
-}
+};
 
 app.setCalculations = (calculationsToSet) => {
   calculations = calculationsToSet;
-}
+};
 
 module.exports = app;
